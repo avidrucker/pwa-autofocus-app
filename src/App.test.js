9adaa0ec17abdc9eceb2fd79be6a39f0cbff6d2d
avidrucker <avidrucker@gmail.com>
2023-10-14 11:46:22 -0400

    tut section 6, add first React test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders add task button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const addButton = screen.getByText(/Add/i);
+  expect(addButton).toBeInTheDocument();
 });
