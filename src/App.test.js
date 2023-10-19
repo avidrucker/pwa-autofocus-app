bfc01d8e46a032b463625da17d0bac60da9deb0c
avidrucker <avidrucker@users.noreply.github.com>
2023-10-19 20:58:45 +0000

    deploy: 466168b789709b90abff22d4c010a4ef6c7300ef

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders add task button', () => {
-  render(<App />);
-  const addButton = screen.getByText(/Add/i);
-  expect(addButton).toBeInTheDocument();
-});
