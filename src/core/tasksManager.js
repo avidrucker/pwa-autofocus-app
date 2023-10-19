110269239e3685c6a3390d000d4959d408c12e0c
avidrucker <avidrucker@gmail.com>
2023-10-19 15:37:55 -0400

    rm toggle task done, refactor for brevity

--- src/core/tasksManager.js
+++ src/core/tasksManager.js
@@ -1,22 +1,12 @@
 // tasksManager.js
 export const nextId = (tasks) => {
-  if(tasks.length === 0) {
-    return 0;
-  } else {
-    return Math.max(...tasks.map(x => x.id)) + 1;
-  }
+  return tasks.length === 0 ? 
+    0 : 
+    Math.max(...tasks.map(x => x.id)) + 1;
 }
 
-export const addTask = (tasks, text) => {
-  return [...tasks, { id: nextId(tasks), text, done: false }];
-};
+export const addTask = (tasks, text) =>
+  [...tasks, 
+    { id: nextId(tasks), text, status: "new" }];
 
-export const toggleTaskDone = (tasks, taskId) => {
-  return tasks.map(task =>
-    task.id === taskId ? { ...task, done: !task.done } : task
-  );
-};
-
-export const emptyList = () => {
-  return [];
-};
+export const emptyList = () => [];
