1b0bb1650eb339f47fd574b759d53966dc943b1d
avidrucker <avidrucker@gmail.com>
2023-10-29 12:36:01 -0400

    fix vert center svg icon on 1st line of item text

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -16,6 +16,7 @@ function TodoItem({ task, isBenchmark }) {
   return (
     <li className={`flex lh-copy align-start ${task.status === "done"
        && "o-50"} ${isBenchmark ? "fw6" : "fw4"}`}>
+      <span className="mr1 dib h-15">{task.status === "done" ? 
                 filledCircle :
                 task.status === "ready" ? 
                     dotCircle : emptyCircle}</span>
