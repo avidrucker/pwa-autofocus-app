7ecc23b54b4d01aee06004405b039305f9eafc5a
avidrucker <avidrucker@gmail.com>
2023-11-14 11:50:25 -0500

    refactor move svg icons to icons module

--- src/App.js
+++ src/App.js
@@ -4,16 +4,9 @@ import { startReview, handleReviewDecision, isPrioritizableList, genQuestion, ge
 import { getFromLocalStorage, saveToLocalStorage } from './core/localStorageAdapter';
 import { exportTasksToJSON, importTasksFromJSON } from './core/tasksIO';
 import TodoItem from './TodoItem';
+import {saveDisk, infoCircle} from './core/icons'
+
 import './App.css';
-
-// TODO: extract out following svg icons into a separate module as per hexagonal architecture
-const infoCircle = <svg fill="currentColor" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-<path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path>
-</svg>;
-
-const saveDisk = <svg fill="currentColor" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-<path d="M433.941 129.941l-83.882-83.882A48 48 0 0 0 316.118 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V163.882a48 48 0 0 0-14.059-33.941zM224 416c-35.346 0-64-28.654-64-64 0-35.346 28.654-64 64-64s64 28.654 64 64c0 35.346-28.654 64-64 64zm96-304.52V212c0 6.627-5.373 12-12 12H76c-6.627 0-12-5.373-12-12V108c0-6.627 5.373-12 12-12h228.52c3.183 0 6.235 1.264 8.485 3.515l3.48 3.48A11.996 11.996 0 0 1 320 111.48z"></path>
-</svg>;
 
 const activeListOffset = 0;
 const queryStringListOffset = 100;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,5 @@
 import React from 'react';
-
-const dotCircle = <svg fill="currentColor" viewBox="0 0 512 512" width="1.25rem" xmlns="http://www.w3.org/2000/svg">
-<path d="M256 56c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m0-48C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 168c-44.183 0-80 35.817-80 80s35.817 80 80 80 80-35.817 80-80-35.817-80-80-80z"></path>
-</svg>;
-
-const emptyCircle = <svg fill="currentColor" viewBox="0 0 512 512" width="1.25rem" xmlns="http://www.w3.org/2000/svg">
-<path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z"></path>
-</svg>;
-
-const filledCircle = <svg fill="currentColor" viewBox="0 0 512 512" width="1.25rem" xmlns="http://www.w3.org/2000/svg">
-<path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path>
-</svg>;
+import {dotCircle, emptyCircle, filledCircle} from './core/icons'
 
 function TodoItem({ task, isBenchmark }) {
   return (
