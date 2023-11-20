c1b026ab9ee3ba991c538095d4606d9e8678b690
avidrucker <avidrucker@gmail.com>
2023-11-19 20:48:11 -0500

    refactor App test to expect console info

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders add task button', () => {
+  // Mock console.info method
+  console.info = jest.fn();
+
   render(<App />);
-  const addButton = screen.getByText(/Add/i);
+  const addButton = screen.getByText(/Add Task/i);
   expect(addButton).toBeInTheDocument();
+
+  // Verify that console.info was called
+  expect(console.info).toHaveBeenCalled();
 });
