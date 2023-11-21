e0787bda467d30937060cf70cb4c8bd0e4ab0673
avidrucker <avidrucker@gmail.com>
2023-11-21 17:03:59 -0500

    test addAll, cancelItem, cloneItem

--- src/core/tasksManager.test.js
+++ src/core/tasksManager.test.js
@@ -1,4 +1,5 @@
-import { addTask, nextId, completeBenchmarkTask } from './tasksManager';
+import { addAll, addTask, nextId, completeBenchmarkTask, 
+  cancelItem, cloneItem } from './tasksManager';
 
 describe('nextId function', () => {
   test('should return 0 for an empty tasks list', () => {
@@ -48,6 +49,12 @@ describe('addTask function', () => {
 });
 
 describe('completeBenchmarkTask function', () => {
+  test('should mark the single ready task as done', () => {
+    const tasks = [{"id": 0, "text": "a", "status": "ready"}];
+    const updatedTasks = completeBenchmarkTask(tasks);
+    expect(updatedTasks).toStrictEqual([{"id": 0, "text": "a", "status": "done"}]);
+  });
+
   test('should mark the benchmark task and automark the new item', () => {
     const tasks = [{"id": 0, "text": "a", "status": "done"},
                   {"id": 1, "text": "b", "status": "ready"},
@@ -56,5 +63,47 @@ describe('completeBenchmarkTask function', () => {
     expect(updatedTasks).toStrictEqual([{"id": 0, "text": "a", "status": "done"},
                                         {"id": 1, "text": "b", "status": "done"},
                                         {"id": 2, "text": "c", "status": "ready"}]);
+  });
+});
+
+describe('cancelItem function', () => {
+  test('should mark an item as cancelled', () => {
+    const tasks = [{ id: 0, text: 'task1', status: "ready" }];
+    const result = cancelItem(tasks, 0);
+    expect(result.length).toBe(1);
+    expect(result[0].status).toBe('cancelled');
+    expect(result[0].was).toBe('ready');
+  });
+
+  test('should trigger the next automarkable item to get automarked', () => {
+    const tasks = [{ id: 0, text: 'task1', status: "ready" },
+                   { id: 1, text: 'task2', status: "new" }];
+    const result = cancelItem(tasks, 0);
+    expect(result.length).toBe(2);
+    expect(result[0].status).toBe('cancelled');
+    expect(result[0].was).toBe('ready');
+    expect(result[1].status).toBe('ready');
+  });
+});
+
+describe('cloneItem function', () => {
+  test('should clone an item and add it to the list', () => {
+    const tasks = [{ id: 0, text: 'task1', status: "done" }];
+    const result = cloneItem(tasks, 0);
+    expect(result.length).toBe(2);
+    expect(result[1]).toStrictEqual({ id: 1, text: 'task1', status: "ready"});
+  });
+});
+
+describe('addAll function', () => {
+  test('should add all items from the new list to the old list', () => {
+    const initialTasks = [{ id: 0, text: 'task1', status: "done" }];
+    const newTasks = [{ id: 1, text: 'task2', status: "ready" },
+                      { id: 2, text: 'task3', status: "new" }];
+    const result = addAll(initialTasks, newTasks);
+    expect(result.length).toBe(3);
+    expect(result[0]).toStrictEqual({ id: 0, text: 'task1', status: "done" });
+    expect(result[1]).toStrictEqual({ id: 1, text: 'task2', status: "ready" });
+    expect(result[2]).toStrictEqual({ id: 2, text: 'task3', status: "new" });
   });
 });
