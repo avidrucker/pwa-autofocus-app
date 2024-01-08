addbfc09d71b02ff7c7e48fc86ba482639fc29f2
avidrucker <avidrucker@gmail.com>
2024-01-08 11:18:06 -0500

    fix breaking test

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import App from './App';
 
 describe('App', () => {
 
-  test('renders add task button', () => {
+  test('renders add item button', () => {
     // Mock console.info method
     console.info = jest.fn();
 
     render(<App />);
-    const addButton = screen.getByText(/Add Task/i);
+    const addButton = screen.getByText(/Add Item/i);
     expect(addButton).toBeInTheDocument();
 
     // Verify that console.info was called
